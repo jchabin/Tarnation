09f787c9b10d23ee62d40c4de48174e96e115bc7
jchabin <jchabin@cherrycreekschools.org>
2017-03-01 08:00:04 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-var ready=false;
-var results;
-function search(query){
+function tarnation(){
+	var query;
+	var results;
 	var s=new XMLHttpRequest();
 	s.open("GET","http://www.faroo.com/api?q="+query+"&key=key");
 	s.onreadystatechange=function(){
 		if(this.readystate==4&&this.status==200){
-			ready=true;
 			results=this.responseText;
+			results=results.results[Math.floor(Math.random()*results.results.length)].iurl;
+			document.getElementById("wot").src=results;
 		}
 	}
 	s.send();
